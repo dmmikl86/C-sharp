3b56a3f1bb4fa086f4bd592860e975b89da558e2
Michael Dmytriaha <mdmytiaha@playtika.com.ua>
2015-07-06 18:39:58 +0300

    addded projects

---
+++ module 01/HomeWork1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Michael";
+            string lastName = "Smit";
+            DateTime birthdate = DateTime.Parse("27.09.1986");
+            string addressLine1 = "Hm.Street 75/52";
+            string addressLine2 = "Space 7/27";
+            string city = "Vinnitsa";
+            string state = "Vinn";
+            int zipCode = 21021;
+            string country = "Ukraine";
+
+            Console.WriteLine("firstName: {0}",firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine(birthdate);
+            Console.WriteLine(addressLine1);
+            Console.WriteLine(addressLine2);
+            Console.WriteLine(city);
+            Console.WriteLine(state);
+            Console.WriteLine(zipCode);
+            Console.WriteLine(country);
+            Console.ReadLine(); // this line will not close console
+        }
+    }
+}
---
+++ module 01/HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static byte X = 0;
+        static byte O = 1;
+        static uint boardSize = 8;
+        static void Main(string[] args)
+        {
+            string line;
+            byte startSymbol = 0;
+            for (int i = 0; i < boardSize; i++)
+            {
+                line = "";
+                if (i % 2 == 1) startSymbol = X; else startSymbol = O;
+                byte currentCell = startSymbol;
+                for (int n = 0; n < boardSize; n++)
+                {
+                    currentCell = invertCell(currentCell);
+                    line += toString(currentCell);
+                }
+                Console.WriteLine(line);
+            }
+            Console.ReadKey();
+        }
+
+        static private byte invertCell(byte current)
+        {
+            if (current == X) return O; else return X;
+        }
+
+        static private string toString( byte currentCell){
+            if (currentCell == X) return "X"; else return "O";
+        }
+    }
+}
