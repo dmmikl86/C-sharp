68eb172609198babbf335059c14bc0e73f664bd9
Michael Dmytriaha <mdmytiaha@playtika.com.ua>
2015-07-08 11:21:53 +0300

    module 3: complete

---
+++ module 03/Homework3/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace Homework3
+{
+    internal class Program
+    {
+        private static Student _student;
+        private static Teacher _teacher;
+
+        private static void Main(string[] args)
+        {
+            _student = new Student();
+            _teacher = new Teacher();
+            GetStudentInformation();
+            GetTeacherInformation();
+
+            Console.WriteLine("press any key to see info");
+            Console.ReadKey();
+            Console.WriteLine();
+            PrintInfo();
+            Console.ReadKey();
+        }
+
+        private static void PrintInfo()
+        {
+            Console.WriteLine(_student.ToString());
+            Console.WriteLine(_teacher.ToString());
+        }
+
+        private static void GetStudentInformation()
+        {
+            Console.WriteLine("Enter the student's first name: ");
+            _student.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter the student's last name: ");
+            _student.LastName = Console.ReadLine();
+            Console.WriteLine("Enter the student birthday separated by dot (.): ");
+            ValidateBirthday(Console.ReadLine());
+        }
+
+        private static void GetTeacherInformation()
+        {
+            Console.WriteLine("Enter the teacher's first name: ");
+            _teacher.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter the teacher's course: ");
+            _teacher.Course = Console.ReadLine();
+            Console.WriteLine("Enter the teacher specialization: ");
+            _teacher.Specialization = Console.ReadLine();
+        }
+
+        private static void ValidateBirthday(string birthday)
+        {
+            DateTime validBirthday;
+            if (DateTime.TryParse(birthday, out validBirthday))
+            {
+                string.Format("{0:d/MM/yyyy}", validBirthday);
+                _student.Birthday = validBirthday;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Date");
+            }
+        }
+
+        #region helperClasses
+
+        private class Student
+        {
+            private string firstName = "";
+            private string lastName = "";
+
+            public Student()
+            {
+                Birthday = new DateTime();
+            }
+
+            public string FirstName
+            {
+                get { return firstName; }
+                set { firstName = value; }
+            }
+
+            public string LastName
+            {
+                get { return lastName; }
+                set { lastName = value; }
+            }
+
+            public DateTime Birthday { get; set; }
+
+            public override string ToString()
+            {
+                return "STUDENT INFO: " + firstName + " " + lastName + " " + Birthday.ToShortDateString();
+            }
+        }
+
+        private class Teacher
+        {
+            private string course = "";
+            private string firstName = "";
+            private string specialization = "";
+
+            public string Course
+            {
+                get { return course; }
+                set { course = value; }
+            }
+
+            public string Specialization
+            {
+                get { return specialization; }
+                set { specialization = value; }
+            }
+
+            public string FirstName
+            {
+                get { return firstName; }
+                set { firstName = value; }
+            }
+
+            public override string ToString()
+            {
+                return "TEACHER INFO: " + firstName + " " + course + " " + specialization;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ module 03/worksheet/worksheet/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace worksheet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#region Methods
+            /*int first = 24;
+            string sValue;
+            string tValue = "";
+            int first34;
+            ReturnMultiOut(out first34, out sValue, tValue);
+
+            sValue = "";
+            ReturnMultiRef(ref first, ref sValue);
+            Console.WriteLine("{0} {1}", first.ToString(), sValue);
+
+            Console.WriteLine("{0}, {1}", first.ToString(), sValue);
+
+            StopService(true, tValue);
+
+            // The method can be called in the normal way, by using positional arguments.
+            Console.WriteLine(CalculateBMI(123, 64));
+
+            // Named arguments can be supplied for the parameters in either order.
+            CalculateBMI(height: 3);
+
+            StopService(true, serviceId: 1);*/
+#endregion
+
+
+            Console.ReadKey();
+        }
+
+        static void ReturnMultiOut(out int i, out string s, string st)
+        {
+            i = 25;
+            s = "using out";
+            st = "tValue";
+
+        }
+
+        static void ReturnMultiRef(ref int i, ref string s)
+        {
+            i = i + 2;
+            s = "using ref";
+        }
+
+        static void StopService(bool forceStop, string serviceName = null, int serviceId = 1)
+        {
+            // code here that will stop the service
+        }
+
+        static int CalculateBMI(int weight = 5, int height = 6)
+        {
+            return (weight * 703) / (height * height);
+        }
+    }
+}
