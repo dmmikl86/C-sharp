f7d85709d3ebf750de713bbfac449fd5d0fb26ad
Michael Dmytriaha <mdmytiaha@playtika.com.ua>
2015-09-07 10:47:42 +0300

    operator overload

--- Programming with C#/module 03/worksheet/worksheet/Program.cs
+++ Programming with C#/module 03/worksheet/worksheet/Program.cs
@@ -10,56 +10,25 @@ namespace worksheet
     {
         static void Main(string[] args)
         {
-#region Methods
-            /*int first = 24;
-            string sValue;
-            string tValue = "";
-            int first34;
-            ReturnMultiOut(out first34, out sValue, tValue);
 
-            sValue = "";
-            ReturnMultiRef(ref first, ref sValue);
-            Console.WriteLine("{0} {1}", first.ToString(), sValue);
-
-            Console.WriteLine("{0}, {1}", first.ToString(), sValue);
-
-            StopService(true, tValue);
-
-            // The method can be called in the normal way, by using positional arguments.
-            Console.WriteLine(CalculateBMI(123, 64));
-
-            // Named arguments can be supplied for the parameters in either order.
-            CalculateBMI(height: 3);
-
-            StopService(true, serviceId: 1);*/
-#endregion
-
-
-            Console.ReadKey();
+            Irt i = new Irt(5);
+            int res = i + 5;
+            Console.WriteLine(res);
         }
 
-        static void ReturnMultiOut(out int i, out string s, string st)
-        {
-            i = 25;
-            s = "using out";
-            st = "tValue";
 
-        }
-
-        static void ReturnMultiRef(ref int i, ref string s)
+        class Irt
         {
-            i = i + 2;
-            s = "using ref";
-        }
+            public int n;
+            public Irt(int n)
+            {
+                this.n = n;
+            }
 
-        static void StopService(bool forceStop, string serviceName = null, int serviceId = 1)
-        {
-            // code here that will stop the service
-        }
 
-        static int CalculateBMI(int weight = 5, int height = 6)
-        {
-            return (weight * 703) / (height * height);
+            public static int operator + (Irt i, int n){
+                return i.n + n;        
+            }
         }
     }
 }
