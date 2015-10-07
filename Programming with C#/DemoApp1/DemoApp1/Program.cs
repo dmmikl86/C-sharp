082bbf4f9adaebaab3c466afef353e26508004f8
Michael Dmytriaha <mdmytiaha@playtika.com.ua>
2015-10-07 13:14:17 +0300

    added demo app1

---
+++ Programming with C#/DemoApp1/DemoApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(3);
+            list.Add(5);
+            foreach (int n in list)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
